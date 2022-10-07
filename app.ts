23ceb6e40a7567639651e96d69ea7879d990e642
gabbar23 <amansaini842@gmail.com>
2022-10-07 14:55:21 +0530

    node basics

--- app.ts
+++ app.ts
@@ -1,5 +1,37 @@
-let a:number;
-a=55;
-console.log(a);
+import http from "http";
+import fs from "fs";
+// function rqListner(req:any,res:any){
 
-console.log("hii")
+// }
+
+const server = http.createServer((req, res) => {
+  console.log(req.headers, req.url, req.method);
+  const method = req.method;
+  if (req.url == "/") {
+    res.write(`<html lang="en">
+  <head>
+      <title>Form</title>
+  </head>
+  <body>
+  <form action="/message" method="post"><input type="text" name="message"> <button type="submit">send</button></form>
+  </body>
+  </html>`);
+    return res.end();
+  }
+  if (req.url == "/message" && method == "POST") {
+    const body: any = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("myfile", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+});
+server.listen(3000);
